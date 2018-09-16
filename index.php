f9bf4ecfb306808d4180b2b3b567c6f4841ca88f
Anatoly <yaAnatolii2016@yandex.ru>
2018-09-16 14:42:51 +0300

    редактирование удаление

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ $link = mysqli_connect('localhost','root','','WD04-filmoteka-andreev');
 if (mysqli_connect_error()){
 	die("ошибка подключения к базе данных");
 } 
- 
+
 /*echo "<pre>";
 print_r($_GET);
 echo "</pre>";*/
@@ -38,7 +38,10 @@ if ( mysqli_affected_rows($link) > 0) {
 } else {
 	 $resulError = "<p>Что-то пошло не так</p>";
 }
+
+
 }
+
 
 $query = "SELECT * From movies"; 
 $movies = array();
@@ -74,6 +77,7 @@ if ($result = mysqli_query($link,$query)){
 ?>
 <body class="index-page">
 	<div class="container user-content section-page">
+
 <?php if ( $resultSuccess != '') {?>
 					<div class="info-success"><?=$resultSuccess?></div>
 <?php  } ?>
@@ -86,10 +90,13 @@ if ($result = mysqli_query($link,$query)){
 					<div class="error"><?=$resultError?></div>
 <?php  } ?>
 
+
+
 		<div class="title-1">Фильмотека</div>
+
 <?php 
   foreach ($movies as $key => $value) {
-  	?> 
+  ?> 
 		<div class="card mb-20">
   <div class="card__header"> 
 			<h4 class="title-4"><?php echo $movies[$key]['name'] ?></h4>
@@ -98,9 +105,7 @@ if ($result = mysqli_query($link,$query)){
    <a  href="index.php?action=delete&id=<?=$value['id']?>" class="button button--delete">Удалить</a> 
    </div>
 		</div>
-?>
-		<div class="card mb-20">
-			<h4 class="title-4"><?php echo $movies[$key]['name'] ?></h4>
+
 			<div class="badge"><?php echo $movies[$key]['kind'] ?></div>
 			<div class="badge"><?php echo $movies[$key]['year'] ?></div>
 		</div>
